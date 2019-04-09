bd58dfb7b201815b52d9a7b131ee599c8dc755c6
Leonardo <leonardobarrosbhz@gmail.com>
2019-04-09 18:36:08 -0300

    buscando dados da api

--- src/components/views/Inventory.vue
+++ src/components/views/Inventory.vue
@@ -1,35 +1,56 @@
   
 <template>
-  <div class="row">
+   <div v-if="!loading" class="row">
     
     
     <div v-for="(item, index) in items" :key="index" class="card" style="width: 13rem;">
-      <img  class="card-img-top" :src="item.photo" alt="Card image Cap">
+      <router-link tag="div" :to="{ path: '/item/' + item.id}">
+          <img  class="card-img-top" :src="item.photo" alt="Card image Cap">
       <div class="card-body">
-        <h5 class="card-title">{{item.title}}</h5>
+        <h5 class="card-title text-center">{{item.title}}</h5>
+      </div>
+
+      </router-link>
+      
+       <div class="card-footer">
         <p class="card-text" >${{item.price}}</p>
-        <a @click="addCart(item)" class="btn btn-primary">+add</a>
+        <a @click="addCart(item)" class="btn btn-sm  btn-primary float-right">+add</a>
       </div>
     </div>
    
     
-  </div>
+  </div> 
+<h1 v-else>Loading ...</h1>
 </template>
 
 
 
 <script>
+
+import axios from 'axios'
+
 export default {
 data(){
   return{
+    loading: true,
     items:[]
   }
 },
-
-
+mounted(){
+   
+  this.fetchInventory();
+},
  methods:{
      addCart(item){
          this.$emit('newItemAdded', item)   
+     },
+     fetchInventory(){
+      var self = this
+       
+       axios.get('http://localhost:3000/items').then(response =>{
+           self.items = response.data,
+            self.loading = false
+        }).catch( () => {alert('erro acessando o Servidor')})
      }
  }
 };
--- src/components/views/ItemDetails.vue
+++ src/components/views/ItemDetails.vue
@@ -1,9 +1,37 @@
 <template>
-    <h3>Details {{$route.params.id}}</h3>
+   <div class="row">
+       <div class="col-sm-6">
+           <img :src="item.photo" alt="photo">
+       </div>
+        <div class="col-sm-6">
+           <h4>{{item.title}}</h4>
+           <p>{{item.description}}</p>
+           <p>{{item.price}}</p>
+       </div>
+
+   </div>
 </template>
 <script>
-export default {
 
+import axios from 'axios'
+export default {
+    
+        data(){
+            return {
+                item:null
+            }
+        },
+        mounted(){
+           this.fetchItem();
+        },
+        methods:{
+            fetchItem(){
+                var self = this;
+                    axios.get('http://localhost:3000/item/' + this.$route.params.id).then(response=>{
+                           self.item = response.data 
+                    })
+            }
+        }
 }
 
 
